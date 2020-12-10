493aa702da168fbaa4077b9813a63d6ce220621f
Hans Petter Naumann <hans_petter.naumann@knowit.no>
2020-12-10 10:13:51 +0100

    .

--- src/helpers/ScoreHelper.js
+++ src/helpers/ScoreHelper.js
@@ -2,22 +2,22 @@ import React, {useEffect, useRef } from 'react';
 
 const ScoreHelper = (function() {
 
-    function getFinalScore(secondsSpent, completedTiles, totalTiles, livesLeft, includeBomb) {   
+    function getFinalScore(secondsSpent, completedTiles, totalTiles, livesLeft, includeBomb) {  
         if(livesLeft == 0)
             livesLeft = 1;
 
         let completedRatio = completedTiles / totalTiles;
 
-        if(completedRatio == 1){
+        if(completedRatio == 1){ 
             completedRatio = 2;
         }
 
         let timeSpentPerTile = secondsSpent / totalTiles;
 
-        let score = (10 * completedRatio) * (100 / timeSpentPerTile) * (totalTiles * 5) ;
+        let score = ((10 * completedRatio) * (100 / timeSpentPerTile) * (totalTiles * 6) / 2);
 
         if(includeBomb){
-            score = score * livesLeft * 1.2;
+            score = score * livesLeft * 1.5;
         }
 
         return Math.floor(score);
